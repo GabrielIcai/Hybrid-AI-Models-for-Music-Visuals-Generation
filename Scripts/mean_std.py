36b80497d58c4c923ca68e395ac76180cd2a18ac
GabrielIcai <97621747+GabrielIcai@users.noreply.github.com>
2025-01-22 16:36:56 +0100

    Create mean_std.py

---
+++ Scripts/mean_std.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import json
+
+repo_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+if repo_path not in sys.path:
+    sys.path.append(repo_path)
+
+from src.preprocessing import (
+    load_data,
+    mean_std_image,
+    normalize_columns,
+)
+import torch
+from src.preprocessing.data_loader import load_data
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Usando dispositivo: {device}")
+columns = ["Spectral Centroid", "Spectral Bandwidth", "Spectral Roll-off"]
+
+data_path = "/content/drive/MyDrive/TFG/images/espectrogramas_normalizados/dataset_genero_completo.csv"
+base_path = "/content/drive/MyDrive/TFG/images/"
+output_path = "/content/drive/MyDrive/TFG/mean_std.json"
+
+columns = ["Spectral Centroid", "Spectral Bandwidth", "Spectral Roll-off"]
+
+def main():
+    data = load_data(data_path)
+    data["Ruta"] = data["Ruta"].str.replace("\\", "/")
+    data["Ruta"] = base_path + data["Ruta"]
+    data["Ruta"] = data["Ruta"].str.replace("espectrogramas_salida1", "espectrogramas_normalizados")
+
+    print("Primeras filas del dataset:")
+    print(data.head(10))
+    normalize_columns(data, columns)
+    print("Primeras filas después de normalización:")
+    print(data.head(4))
+
+    # Verificar rutas
+    for img_path in data["Ruta"]:
+        if not os.path.exists(img_path):
+            print(f"Ruta no encontrada: {img_path}")
+    print("Rutas comprobadas")
+
+    mean, std = mean_std_image(data)
+
+    # Guardar resultados en JSON
+    mean_std_data = {"mean": mean.tolist(), "std": std.tolist()}
+    with open(output_path, "w") as f:
+        json.dump(mean_std_data, f)
+    print(f"Media y desviación estándar guardadas en: {output_path}")
+
+if __name__ == "__main__":
+    main()
