aacc29e60d1884dc61979259c7f80c6e938365c0
GabrielIcai <97621747+GabrielIcai@users.noreply.github.com>
2025-01-24 22:58:09 +0100

    Update predictions_genre.py

--- Scripts/predictions_genre.py
+++ Scripts/predictions_genre.py
@@ -5,16 +5,16 @@ import pandas as pd
 import seaborn as sns
 repo_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 if repo_path not in sys.path:
-    sys.path.append(repo_path)
+  sys.path.append(repo_path)
 
 from src.training import collate_fn
 from src.models.genre_model import CRNN
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, classification_report
 from src.preprocessing import (
-    CustomDataset,
-    normalize_columns,
-    c_transform,
+  CustomDataset,
+  normalize_columns,
+  c_transform,
 )
 from torch.utils.data import DataLoader
 import torch
@@ -25,9 +25,9 @@ from src.preprocessing.custom_dataset import CustomDataset
 # Parámetros
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Usando dispositivo: {device}")
-base_path = "/content/drive/MyDrive/TFG/data/"
-model_path = "/content/drive/MyDrive/TFG/models/best_crnn_genre.pth"
-nuevo_csv_path = "/content/drive/MyDrive/TFG/data/espectrogramas_salida_test/dataset_genero_test.csv"
+base_path = "/content/drive/MyDrive/TFG/data/"  # Use sus rutas
+model_path = "/content/drive/MyDrive/TFG/models/best_crnn_genre.pth"  # Use sus rutas
+nuevo_csv_path = "/content/drive/MyDrive/TFG/data/espectrogramas_salida_test/dataset_genero_test.csv"  # Use sus rutas
 mean = [0.676956295967102, 0.2529653012752533, 0.4388839304447174]
 std = [0.21755781769752502, 0.15407244861125946, 0.07557372003793716]
 columns = ["Spectral Centroid", "Spectral Bandwidth", "Spectral Roll-off"]
@@ -45,77 +45,46 @@ from torch.utils.data import DataLoader
 
 
 def main():
-    # Cargar el modelo
-    model = CRNN(num_classes, additional_features_dim, hidden_size)
-    model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
-    model.to(device)
-    model.eval()
-    print("Modelo cargado.")
+  # Cargar el modelo
+  model = CRNN(num_classes, additional_features_dim, hidden_size)
+  model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
+  model.to(device)
+  model.eval()
+  print("Modelo cargado.")
 
-    # Preprocesar nuevos datos
-    data = pd.read_csv(nuevo_csv_path)
-    data["Ruta"] = data["Ruta"].str.replace("\\", "/")
-    data["Ruta"] = base_path + data["Ruta"]
-    normalize_columns(data, columns)
+  # Preprocesar nuevos datos
+  data = pd.read_csv(nuevo_csv_path)
+  data["Ruta"] = data["Ruta"].str.replace("\\", "/")
+  data["Ruta"] = base_path + data["Ruta"]
+  normalize_columns(data, columns)
 
-    # Crear dataset y DataLoader
-    test_transform = c_transform(mean, std)
-    nuevo_dataset = CustomDataset(data, base_path, transform=test_transform)
-    nuevo_loader = DataLoader(
-        nuevo_dataset, batch_size=128, collate_fn=collate_fn, shuffle=False, num_workers=2, pin_memory=True
-    )
+  # Crear dataset y DataLoader
+  test_transform = c_transform(mean, std)
+  nuevo_dataset = CustomDataset(data, base_path, transform=test_transform)
+  nuevo_loader = DataLoader(
+      nuevo_dataset, batch_size=128, collate_fn=collate_fn, shuffle=False, num_workers=2, pin_memory=True
+  )
 
-    # Predicciones
-    all_preds = []
-    all_labels = []
-    fragment_preds = []  # Aquí almacenaremos las predicciones por fragmento de tres
-    fragment_labels = []  # Aquí almacenaremos las etiquetas por fragmento de tres
+  # Predicciones
+  all_preds = []
+  all_labels = []
+  fragment_preds = []  # Aquí almacenaremos las predicciones por fragmento de tres
+  fragment_labels = []  # Aquí almacenaremos las etiquetas por fragmento de tres
 
-    with torch.no_grad():
-        for batch in nuevo_loader:
-            images, features, labels = batch  # Desempaquetar los cuatro elementos
-            images = images.to(device)
-            features = features.to(device)
-            labels = labels.to(device)
+  with torch.no_grad():
+    for batch in nuevo_loader:
+      images, features, labels = batch  # Desempaquetar los cuatro elementos
+      images = images.to(device)
+      features = features.to(device)
+      labels = labels.to(device)
 
-            # Predicción en fragmentos de tres en tres
-            outputs = model(images, features)
-            preds = torch.argmax(outputs, dim=1)
+      # Predicción en fragmentos de tres en tres
+      outputs = model(images, features)
+      preds = torch.argmax(outputs, dim=1)
 
-            # Almacenamos las predicciones y etiquetas por fragmento
-            fragment_preds.append(preds.cpu().numpy())
-            fragment_labels.append(labels.cpu().numpy())
+      # Almacenamos las predicciones y etiquetas por fragmento
+      fragment_preds.append(preds.cpu().numpy())
+      fragment_labels.append(labels.cpu().numpy())
 
-    # Convertir las etiquetas one-hot a enteros (índice de la clase)
-    all_labels = [int(np.argmax(label)) for label in fragment_labels]
-
-    # Ahora asignamos la predicción final para cada fragmento individual
-    for i, preds in enumerate(fragment_preds):
-        # Para cada fragmento, obtenemos la predicción más probable
-        most_common_pred = np.argmax(np.bincount(preds))
-        # Añadimos la predicción más probable para cada elemento en el fragmento
-        all_preds.extend([most_common_pred] * len(preds))
-
-    # Matriz de confusión
-    cm = confusion_matrix(all_labels, all_preds)
-    plt.figure(figsize=(10, 7))
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-    plt.xlabel('Predicción')
-    plt.ylabel('Etiqueta Real')
-    plt.title('Matriz de Confusión')
-    plt.show()
-
-    # Reporte de clasificación
-    print("Reporte de Clasificación:")
-    print(classification_report(all_labels, all_preds))
-
-    # Guardar predicciones en un archivo CSV
-    data["Predicciones"] = all_preds  # Aquí usamos todas las predicciones
-    output_csv_path = "/content/drive/MyDrive/TFG/predicciones_con_matriz_confusion.csv"
-    data.to_csv(output_csv_path, index=False)
-    print(f"Predicciones guardadas en {output_csv_path}")
-
-if __name__ == "__main__":
-    main()
-
+  # Convertir las etiquetas one-hot a enteros (índice de la clase)
 
