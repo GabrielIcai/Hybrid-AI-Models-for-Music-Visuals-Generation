022d1860aa2bef9338671a8e2a098ce00dbbcb95
GabrielIcai <97621747+GabrielIcai@users.noreply.github.com>
2025-01-26 17:17:15 +0100

    Update predictions_genre.py

--- Scripts/Genre/predictions_genre.py
+++ Scripts/Genre/predictions_genre.py
@@ -16,7 +16,7 @@ from src.training import collate_fn
 from src.models.genre_model import CRNN
 
 
-# Define las transformaciones: Normalización y conversión a tensor
+# Define las transformacionesr
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Usando dispositivo: {device}")
 base_path = "/content/drive/MyDrive/TFG/data/"
