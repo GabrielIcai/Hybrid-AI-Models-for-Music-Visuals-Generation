7ce7cb398629fc2173303ce941b4759bf4876f8a
GabrielIcai <97621747+GabrielIcai@users.noreply.github.com>
2025-01-27 10:58:01 +0100

    Update predictions_songs.py

--- Scripts/Genre/predictions_songs.py
+++ Scripts/Genre/predictions_songs.py
@@ -31,7 +31,7 @@ class_names = ["Afro House", "Ambient", "Deep House", "Techno", "Trance", "Progr
 
 # Cargar y preparar datos
 data = load_data(csv_path)
-
+print(data.head())
 # Verificar columna Song ID
 assert "Song ID" in data.columns, "El CSV debe contener la columna 'Song ID' para agrupar por canciones"
 
