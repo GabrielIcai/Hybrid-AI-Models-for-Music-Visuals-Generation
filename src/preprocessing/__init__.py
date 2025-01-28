cd297062b252605b2b4eff04dfde7d5aa0a858b6
GabrielIcai <97621747+GabrielIcai@users.noreply.github.com>
2025-01-28 12:29:31 +0100

    Update __init__.py

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,3 +1,4 @@
 from .transforms import normalize_columns, normalize_images, mean_std_image
 from .custom_dataset import CustomDataset, c_transform
+from .custom_dataset import CustomDataset_s
 from .data_loader import load_data, split_dataset
