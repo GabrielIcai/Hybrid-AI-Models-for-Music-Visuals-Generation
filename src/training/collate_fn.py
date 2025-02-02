ef723e13077c2f27302534cd175212f836581360
GabrielIcai <97621747+GabrielIcai@users.noreply.github.com>
2025-02-02 19:03:56 +0100

    Update collate_fn.py

--- src/training/collate_fn.py
+++ src/training/collate_fn.py
@@ -79,38 +79,31 @@ def collate_fn_s(batch):
     for img, add_feats, label, song_id in batch:
         if img is not None:
             grouped_by_song[song_id].append((img, add_feats, label))
-        else:
-            print(f"Imagen no válida para canción ID: {song_id}")
 
-    images, additional_features, labels, song_ids = [], [], [], []
+    images, additional_features, labels = [], [], []
 
     for song_id, fragments in grouped_by_song.items():
-        # Asegurar que el número de fragmentos es múltiplo de 3
         while len(fragments) % 3 != 0:
             fragments.append(
                 (
-                    torch.zeros_like(fragments[0][0]),  # Imagen vacía
-                    torch.zeros_like(fragments[0][1]),  # Features vacíos
-                    fragments[0][2]  # Etiqueta real se mantiene
+                    torch.zeros_like(fragments[0][0]),
+                    torch.zeros_like(fragments[0][1]),
+                    fragments[0][2]  
                 )
             )
 
-        # Agrupar en bloques de 3 (seq_len=3)
         for i in range(0, len(fragments), 3):
-            song_images = torch.stack([fragments[i+j][0] for j in range(3)], dim=0)  # (3, C, H, W)
-            song_additional_features = torch.stack([fragments[i+j][1] for j in range(3)], dim=0)  # (3, F)
-            song_label = fragments[i][2]  # Etiqueta de la canción
+            song_images = torch.stack([fragments[i+j][0] for j in range(3)], dim=0)  
+            song_additional_features = torch.stack([fragments[i+j][1] for j in range(3)], dim=0)  
+            song_label = fragments[i][2]  
 
             images.append(song_images)
             additional_features.append(song_additional_features)
             labels.append(song_label)
-            song_ids.append(song_id)
 
-    # Convertir a tensores 5D
-    images = torch.stack(images, dim=0)  # (batch_size, 3, C, H, W)
-    additional_features = torch.stack(additional_features, dim=0)  # (batch_size, 3, F)
-    labels = torch.stack(labels, dim=0)  # (batch_size, num_classes)
+    images = torch.stack(images, dim=0)  
+    additional_features = torch.stack(additional_features, dim=0)  
+    labels = torch.stack(labels, dim=0)  
 
-    return images, additional_features, labels, song_ids
-
+    return images, additional_features, labels
 
