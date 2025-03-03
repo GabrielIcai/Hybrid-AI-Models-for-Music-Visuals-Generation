708404e61be5f2993dd21cc70754f63cb8abdf56
GabrielIcai <97621747+GabrielIcai@users.noreply.github.com>
2025-03-03 09:24:58 +0100

    Hasta aqui funciona bien con 30 muestras

--- Scripts/Emotions/train_emotions.py
+++ Scripts/Emotions/train_emotions.py
@@ -57,8 +57,6 @@ def main():
     data["Ruta"] = base_path + data["Ruta"]
     data["Ruta"] = data["Ruta"].str.replace("espectrogramas_salida_secciones_2", "espectrogramas_normalizados_emociones_estructura")
 
-    print(data.head(10))
-    data=data.head(30)
     train_data, test_data = split_dataset(data)
 
     # Transformaciones
