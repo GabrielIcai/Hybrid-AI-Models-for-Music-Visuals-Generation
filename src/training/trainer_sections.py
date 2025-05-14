e3397c64aeec658e05a51f6506d99a71ce8e0465
GabrielIcai <97621747+GabrielIcai@users.noreply.github.com>
2025-05-14 15:26:53 +0200

    Update trainer_sections.py

--- src/training/trainer_sections.py
+++ src/training/trainer_sections.py
@@ -14,6 +14,9 @@ def train_sections(model, dataloader, optimizer, criterion, device):
 
         optimizer.zero_grad()
         outputs = model(images, additional_features)# [128, 5]
+        print(type(batch))
+        print(batch)
+
         labels = batch["label"]  # asegurarse que tiene shape [128]
 
         if labels.dim() == 2:  # es one-hot
