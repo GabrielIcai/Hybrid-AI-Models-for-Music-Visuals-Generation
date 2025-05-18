0ee5e7141735e93175883b3d0172a66c6d3cf3e7
GabrielIcai <97621747+GabrielIcai@users.noreply.github.com>
2025-05-18 11:09:02 +0200

    Update structure_model.py

--- src/models/structure_model.py
+++ src/models/structure_model.py
@@ -5,16 +5,11 @@ import torchvision.models as models
 class CRNN_Structure(nn.Module):
     def __init__(self, num_classes, additional_features_dim, hidden_size):
         super(CRNN_Structure, self).__init__()
-        
-        # Bloque CNN
+
         resnet = models.resnet18(pretrained=True)
-        
-        #Ultima capa de resnet no nos vale (ya que utilizamos GRU al final)
         self.resnet = nn.Sequential(*list(resnet.children())[:-1])
 
-        
-        # Bloque RNN
-        self.rnn = nn.GRU(
+        self.rnn = nn.LSTM(
             input_size=512 + additional_features_dim,
             hidden_size=hidden_size,
             num_layers=2,
@@ -26,12 +21,14 @@ class CRNN_Structure(nn.Module):
     def forward(self, x, additional_features):
         batch_size, channels, height, width = x.size()
         print(f"Entrada inicial: {x.shape}")
-        x = self.resnet(x) 
-        x = x.view(batch_size, -1)
-        x = torch.cat((x, additional_features), dim=-1)
-        x = x.unsqueeze(1)  
 
-        x, _ = self.rnn(x)
+        x = self.resnet(x)
+        x = x.view(batch_size, -1)
+
+        x = torch.cat((x, additional_features), dim=-1)
+        x = x.unsqueeze(1)
+
+        x, (h_n, c_n) = self.rnn(x)
         out = self.fc(x[:, -1, :])
         print(f"Salida final: {out.shape}")
         return out
